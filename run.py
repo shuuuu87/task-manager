43132fb02fd6e68434938486b8428dc014f94e8d
Shreyash12345678 <shreyashmodkule121@gmail.com>
2025-05-09 20:31:51 +0530

    Added Procfile and updated run.py & requirements

--- run.py
+++ run.py
@@ -5,10 +5,11 @@ from app.models import User, Task
 app = create_app()
 migrate = Migrate(app, db)
 
-# Optional: So you can access these models in `flask shell`
+# Optional: Helps with flask shell usage
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Task': Task}
 
+# This block is safe for local dev; Render will ignore it
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
