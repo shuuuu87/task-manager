6b92f57bae5e9befc18b72accfe99b75c76ba37f
Shreyash12345678 <shreyashmodkule121@gmail.com>
2025-05-09 18:32:48 +0530

    Tracked new files and committed changes

--- app/forms.py
+++ app/forms.py
@@ -6,6 +6,11 @@ class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[
+        DataRequired(),
+        EqualTo('password', message='Passwords must match')
+    ])
+    
     submit = SubmitField('Register')
 
 class LoginForm(FlaskForm):
