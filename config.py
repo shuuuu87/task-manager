f45e38745365ebf2fb8a3cb2cf204fb59603578d
Shreyash12345678 <shreyashmodkule121@gmail.com>
2025-05-13 11:49:56 +0530

    Final config for Render deployment

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+basedir = os.path.abspath(os.path.dirname(__file__))  # ✅ correct
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance', 'task_manager.db')
+    
+    # On Render, use DATABASE_URL from environment
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'instance', 'task_manager.db')
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
